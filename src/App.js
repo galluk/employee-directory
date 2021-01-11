fcd4781f547c05d2e3e0764e82e1604a42925de4
HomeAlone <luke.gallagher.cst@gmail.com>
2021-01-11 14:52:27 +1100

    Add base html components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 // import logo from 'logo.svg';
 import EmployeeResultContainer from "./components/EmployeeResultContainer";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
 
 function App() {
-  return <EmployeeResultContainer />;
+  return (
+    <div className="container">
+      <Navbar />
+      <Jumbotron />
+      <EmployeeResultContainer />
+    </div>
+  );
+  // return <EmployeeResultContainer />;
 }
 
 export default App;
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h1>Employee Directory</h1>
+      <p>
+        Here is a list of the company employees.
+      </p>
+    </div>
+  );
+}
+
+export default Jumbotron;
