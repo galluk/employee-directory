95913b7f5e3c1cc957b2f3ddcb263a228274db67
HomeAlone <luke.gallagher.cst@gmail.com>
2021-01-13 15:19:03 +1100

    changed display to bootstrap table

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,15 +1,40 @@
 import React from "react";
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
 
 function EmployeeList(props) {
+  
+  function imageFormatter(cell, row){
+    return (<img src={""+cell+""} alt=" " />);
+  }
+
+  // set up columns for the bootstrap table
+  const tableColumns = [
+    { dataField: 'img', text: 'Image', formatter: imageFormatter },
+    { dataField: 'firstName', text: 'First Name', sort: true },
+    { dataField: 'lastName', text: 'Last Name', sort: true },
+    { dataField: 'email', text: 'Email', sort: true }
+  ];  
+  
+  // map the given array to the bootstrap table array
+  const tableData = props.results.map(result => ({
+    img: result.picture.medium,
+    firstName: result.name.first,
+    lastName: result.name.last,
+    email: result.email
+  }));  
+
+  console.log(tableData);
+
   return (
-    <ul className="list-group">
-      {props.results.map(result => (
-        <li className="list-group-item" key={result.email}>
-          <img alt={result.name.first + ' ' + result.name.last} className="img-fluid" src={result.picture.thumbnail} />
-          <p>{result.name.first + ' ' + result.name.last}</p>
-        </li>
-      ))}
-    </ul>
+
+    <BootstrapTable 
+      bootstrap4
+      keyField='email' 
+      data={ tableData } 
+      columns={ tableColumns } 
+      >
+    </BootstrapTable>
   );
 }
 
