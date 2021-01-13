4d68cd65f14eebe989afbd1c909787700d735dec
HomeAlone <luke.gallagher.cst@gmail.com>
2021-01-13 15:19:43 +1100

    add filter functionality

--- src/components/EmployeeResultContainer.js
+++ src/components/EmployeeResultContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import FilterForm from "./FilterForm";
 import EmployeeList from "./EmployeeList";
 import API from "../utils/API";
 
 class EmployeeResultContainer extends Component {
   state = {
-    search: "",
-    empResults: []
+    empResults: [],
+    filter: ""
   };
 
   // When this component mounts, get the random employees
@@ -23,10 +24,47 @@ class EmployeeResultContainer extends Component {
     .catch(err => console.log(err));
   };
 
+  filterEmployeesByLastname(match) {  
+    if (match !== '') {
+      // allow for case sensitivity in search
+      const matchRegExp = new RegExp(match, 'i');
+      return this.state.empResults.filter(e => matchRegExp.test(e.name.last));
+    }
+    else {
+      return this.state.empResults;
+    }
+  }
+
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+    console.log('setting filter to: ' + this.state.filter);
+    
+  };
+
+  // When the form is submitted, filter the employee array on last name
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log('filtering by ' + this.state.filter);
+    
+    this.filterEmployeesByLastname(this.state.filter);
+  };
+
   render() {
+    const filteredEmployees = this.filterEmployeesByLastname(this.state.filter);
+
     return (
       <div>
-        <EmployeeList results={this.state.empResults} />
+        <FilterForm
+          filter={this.state.filter}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        {/* <EmployeeList results={this.state.empResults} /> */}
+        <EmployeeList results={filteredEmployees} />
       </div>
     );
   }
