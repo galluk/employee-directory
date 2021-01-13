37c3d3f952d66286420158502ecd2e159ee05c78
HomeAlone <luke.gallagher.cst@gmail.com>
2021-01-13 15:18:16 +1100

    removed navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 // import logo from 'logo.svg';
 import EmployeeResultContainer from "./components/EmployeeResultContainer";
-import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 
 function App() {
   return (
     <div className="container">
-      <Navbar />
       <Jumbotron />
       <EmployeeResultContainer />
     </div>
   );
-  // return <EmployeeResultContainer />;
 }
 
 export default App;
